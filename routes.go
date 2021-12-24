7f7c395d1fec3630025f7650d73e1e8c512b06bb
Mayowa-Ojo <mayowaojo.e@gmail.com>
2021-12-24 08:45:07 -0700

    feat: setup api routes

--- routes.go
+++ routes.go
@@ -1,5 +1,23 @@
 package main
 
-func registerRoutes() {
+import (
+	"net/http"
 
+	"github.com/gin-gonic/gin"
+)
+
+func registerRoutes(router *gin.Engine) {
+	// health-check
+	router.GET("/health", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "ok",
+		})
+	})
+
+	// API v1
+	{
+		v1 := router.Group("/api/v1")
+
+		v1.GET("/", func(c *gin.Context) {})
+	}
 }
