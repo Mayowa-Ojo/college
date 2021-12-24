84841cae2f2676e7b712b36e87557d6d3c95049a
Mayowa-Ojo <mayowaojo.e@gmail.com>
2021-12-24 08:44:05 -0700

    config: database connection and migration

---
+++ db.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"context"
+	"ent-demo/ent"
+	"ent-demo/ent/migrate"
+	"fmt"
+	"log"
+
+	_ "github.com/lib/pq"
+)
+
+func NewEntClient(cfg *Config) *ent.Client {
+	dsn := fmt.Sprintf(
+		"postgres://%s:%s@%s:%s/%s?sslmode=disable",
+		cfg.DBUser, cfg.DBPassword, cfg.DBHost, cfg.DBPort, cfg.DBName,
+	)
+
+	client, err := ent.Open(cfg.DBDriver, dsn)
+	if err != nil {
+		log.Fatalf("[ENT]: error connecting to db %s", err)
+	}
+
+	log.Println("[ENT]: connected to database")
+
+	return client
+}
+
+func SchemaMigrateUp(ctx context.Context, client *ent.Client) {
+	if err := client.Schema.Create(ctx, migrate.WithGlobalUniqueID(true)); err != nil {
+		log.Fatalf("[ENT]: error running migration %s", err)
+	}
+}
+
+func SchemaMigrateDown(ctx context.Context, client *ent.Client) {
+	if err := client.Schema.Create(
+		ctx,
+		migrate.WithDropIndex(true),
+		migrate.WithDropColumn(true),
+	); err != nil {
+		log.Fatalf("[ENT]: error running migration %s", err)
+	}
+}
