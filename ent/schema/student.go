2e4e73d47ad11d587330c3dfc7f740df0121ec40
Mayowa-Ojo <mayowaojo.e@gmail.com>
2021-12-23 19:11:57 -0700

    feat: generate ent assets

---
+++ ent/schema/student.go
@@ -0,0 +1,24 @@
+package schema
+
+import (
+	"entgo.io/ent"
+	"entgo.io/ent/schema/field"
+)
+
+// Student holds the schema definition for the Student entity.
+type Student struct {
+	ent.Schema
+}
+
+// Fields of the Student.
+func (Student) Fields() []ent.Field {
+	return []ent.Field{
+		field.String("firstname"),
+		field.String("lastname"),
+	}
+}
+
+// Edges of the Student.
+func (Student) Edges() []ent.Edge {
+	return nil
+}
