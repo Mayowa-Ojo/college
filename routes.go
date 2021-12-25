33fccdddd8ea0e3faee4b12e431d84dec1d73426
Mayowa-Ojo <mayowaojo.e@gmail.com>
2021-12-25 12:35:41 -0700

    feat: register routes

--- routes.go
+++ routes.go
@@ -1,16 +1,27 @@
-package main
+package college
 
 import (
+	"college/ent"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
 )
 
-func registerRoutes(router *gin.Engine) {
+func registerRoutes(router *gin.Engine, client *ent.Client) {
+	studentRepository := NewStudentService(client)
+	studentHandler := NewStudentHandler(studentRepository)
+
+	departmentRepository := NewDepartmentService(client)
+	departmentHandler := NewDepartmentHandler(departmentRepository)
+
+	facade := NewFacade(studentHandler, departmentHandler)
+
 	// health-check
 	router.GET("/health", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{
-			"message": "ok",
+			"status":  "ok",
+			"message": "up and running...",
+			"data":    nil,
 		})
 	})
 
@@ -18,6 +29,16 @@ func registerRoutes(router *gin.Engine) {
 	{
 		v1 := router.Group("/api/v1")
 
-		v1.GET("/", func(c *gin.Context) {})
+		v1.GET("/students", facade.GetStudents)
+		v1.POST("/students", facade.CreateStudent)
+
+		v1.POST("/departments", facade.CreateDepartment)
 	}
+
+	router.NoRoute(func(c *gin.Context) {
+		c.JSON(http.StatusNotFound, gin.H{
+			"status":  "error",
+			"message": "resource does not exist",
+		})
+	})
 }
