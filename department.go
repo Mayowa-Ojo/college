f3a6d40979e27bc8e71105ef97fa9115bc4b23fe
Mayowa-Ojo <mayowaojo.e@gmail.com>
2021-12-26 10:23:19 -0700

    feat: implement methods for usecase and repository layer

--- department.go
+++ department.go
@@ -1,11 +1,123 @@
-package main
+package college
 
-import "ent-demo/ent"
+import (
+	"college/ent"
+	"college/ent/department"
+	"context"
+	"time"
+
+	"github.com/google/uuid"
+)
+
+/**
+Model
+*/
+type Department struct {
+	ID        uuid.UUID `json:"id"`
+	Name      string    `json:"name"`
+	Code      string    `json:"code"`
+	Telephone string    `json:"telephone"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+}
+
+func parseToDepartment(model *ent.Department) *Department {
+	return &Department{
+		ID:        model.ID,
+		Name:      model.Name,
+		Code:      model.Code,
+		Telephone: model.Telephone,
+		CreatedAt: model.CreatedAt,
+		UpdatedAt: model.UpdatedAt,
+	}
+}
 
 func NewDepartmentService(client *ent.Client) DepartmentRepository {
 	return &Datastore{c: client}
 }
 
-func (d *Datastore) FindListDepartments() {
-	d.c.Student.Create()
+/**
+Repository
+*/
+func (d *Datastore) FindListDepartments(ctx context.Context) ([]*ent.Department, error) {
+	entities, err := d.c.Department.Query().All(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	return entities, nil
+}
+
+func (d *Datastore) FindListDepartmentsWithStudents(ctx context.Context) ([]*ent.Department, error) {
+	entities, err := d.c.Department.Query().WithStudents().All(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	return entities, nil
+}
+
+func (d *Datastore) FindDepartmentByID(ctx context.Context, ID uuid.UUID) (*ent.Department, error) {
+	entity, err := d.c.Department.Get(ctx, ID)
+	if err != nil {
+		return nil, err
+	}
+
+	return entity, nil
+}
+
+func (d *Datastore) FindDepartmentByName(ctx context.Context, name string) (*ent.Department, error) {
+	entity, err := d.c.Department.Query().Where(department.NameEqualFold(name)).First(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	return entity, nil
+}
+
+func (d *Datastore) CreateDepartment(ctx context.Context, dpt *Department) (*ent.Department, error) {
+	entity, err := d.c.Department.
+		Create().
+		SetName(dpt.Name).
+		SetTelephone(dpt.Telephone).
+		SetCode(dpt.Code).
+		Save(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	return entity, nil
+}
+
+/**
+Handlers
+*/
+type DepartmentHandler struct {
+	dr DepartmentRepository
+}
+
+func NewDepartmentHandler(dr DepartmentRepository) *DepartmentHandler {
+	return &DepartmentHandler{dr}
+}
+
+func (dh *DepartmentHandler) GetDepartments() {}
+
+func (dh *DepartmentHandler) CreateDepartment(ctx context.Context, vm CreateDepartmentVM) (*Department, error) {
+	_, err := dh.dr.FindDepartmentByName(ctx, vm.Name)
+	if err == nil {
+		return nil, ErrDepartmentAlreadyExists
+	}
+
+	d := &Department{
+		Name:      vm.Name,
+		Code:      vm.Code,
+		Telephone: vm.Telephone,
+	}
+
+	entity, err := dh.dr.CreateDepartment(ctx, d)
+	if err != nil {
+		return nil, err
+	}
+
+	return parseToDepartment(entity), nil
 }
--- student.go
+++ student.go
@@ -1,56 +1,231 @@
-package main
+package college
 
 import (
+	"college/ent"
+	"college/ent/student"
 	"context"
-	"ent-demo/ent"
 	"time"
+
+	"github.com/google/uuid"
+	nanoid "github.com/matoous/go-nanoid/v2"
 )
 
 /**
 Model
 */
 type Student struct {
-	ID              int       `json:"id"`
-	Firstname       string    `json:"first_name"`
-	Lastname        string    `json:"last_name"`
-	Email           string    `json:"email"`
-	AdmissionNumber string    `json:"admission_number"`
-	Year            int       `json:"year"`
-	CreatedAt       time.Time `json:"created_at"`
-	UpdatedAt       time.Time `json:"updated_at"`
+	ID              uuid.UUID   `json:"id"`
+	Firstname       string      `json:"firstname"`
+	Lastname        string      `json:"lastname"`
+	Email           string      `json:"email"`
+	AdmissionNumber string      `json:"admission_number"`
+	Year            int         `json:"year"`
+	Department      *Department `json:"department"`
+	CreatedAt       time.Time   `json:"created_at"`
+	UpdatedAt       time.Time   `json:"updated_at"`
 }
 
 func parseToStudent(model *ent.Student) *Student {
-	return nil
-}
+	var d *Department
 
-type StudentHandler struct {
-	sr StudentRepository
+	if model.Edges.Department != nil {
+		d = parseToDepartment(model.Edges.Department)
+	}
+
+	return &Student{
+		ID:              model.ID,
+		Firstname:       model.Firstname,
+		Lastname:        model.Lastname,
+		Email:           model.Email,
+		AdmissionNumber: model.AdmissionNumber,
+		Year:            model.Year,
+		Department:      d,
+		CreatedAt:       model.CreatedAt,
+		UpdatedAt:       model.UpdatedAt,
+	}
 }
 
 func NewStudentService(client *ent.Client) StudentRepository {
 	return &Datastore{c: client}
 }
 
-func NewStudentHandler(sr StudentRepository) *StudentHandler {
-	return &StudentHandler{sr}
-}
-
 /**
 Repository
 */
-func (d *Datastore) FindListStudents(ctx context.Context) ([]*ent.Student, error) {
-	entities, err := d.c.Student.Query().All(ctx)
+func (d *Datastore) FindListStudents(ctx context.Context, pg *Paginator) ([]*ent.Student, *Paginator, error) {
+	total, err := d.c.Student.Query().Count(ctx)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	pg.Total = total
+	pg.build(total)
+
+	offset := pg.getOffset()
+	limit := pg.PageCount
+
+	entities, err := d.c.Student.
+		Query().
+		WithDepartment().
+		Order(ent.Desc(student.FieldCreatedAt)).
+		Limit(limit).
+		Offset(offset).All(ctx)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	return entities, pg, nil
+}
+
+func (d *Datastore) FindStudentByEmail(ctx context.Context, email string) (*ent.Student, error) {
+	entity, err := d.c.Student.Query().Where(student.EmailEQ(email)).First(ctx)
 	if err != nil {
 		return nil, err
 	}
 
-	return entities, nil
+	return entity, nil
+}
+
+func (d *Datastore) CreateStudent(ctx context.Context, s *Student) (*ent.Student, error) {
+	entity, err := d.c.Student.
+		Create().
+		SetFirstname(s.Firstname).
+		SetLastname(s.Lastname).
+		SetEmail(s.Email).
+		SetAdmissionNumber(s.AdmissionNumber).
+		SetYear(s.Year).
+		Save(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	return entity, nil
+}
+
+func (d *Datastore) UpdateStudentDetails(ctx context.Context, s *Student, ID uuid.UUID) (*ent.Student, error) {
+	entity, err := d.c.Student.
+		UpdateOneID(ID).
+		SetFirstname(s.Firstname).
+		SetLastname(s.Lastname).
+		SetEmail(s.Email).
+		Save(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	return entity, nil
+}
+
+func (d *Datastore) UpdateStudentDepartment(ctx context.Context, studentID, departmentID uuid.UUID) (*ent.Student, error) {
+	entity, err := d.c.Student.
+		UpdateOneID(studentID).
+		SetDepartmentID(departmentID).
+		Save(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	return entity, nil
+}
+
+func (d *Datastore) DeleteStudent(ctx context.Context, ID uuid.UUID) error {
+	if err := d.c.Student.DeleteOneID(ID).Exec(ctx); err != nil {
+		return err
+	}
+
+	return nil
 }
 
 /**
 Handlers
 */
-func (sh *StudentHandler) GetStudents(ctx context.Context) {
-	sh.sr.FindListStudents(ctx)
+type StudentHandler struct {
+	sr StudentRepository
+}
+
+func NewStudentHandler(sr StudentRepository) *StudentHandler {
+	return &StudentHandler{sr}
+}
+
+func (sh *StudentHandler) GetStudents(ctx context.Context, vm GetStudentsVM) ([]*Student, *Paginator, error) {
+	paginator := NewPaginator(WithPaginatorCount(vm.Count), WithPaginatorPage(vm.Page))
+
+	entities, paginator, err := sh.sr.FindListStudents(ctx, paginator)
+	if err != nil {
+		return nil, paginator, err
+	}
+
+	var students []*Student
+
+	for _, v := range entities {
+		students = append(students, parseToStudent(v))
+	}
+
+	return students, paginator, nil
+}
+
+func (sh *StudentHandler) CreateStudent(ctx context.Context, vm CreateStudentVM) (*Student, error) {
+	_, err := sh.sr.FindStudentByEmail(ctx, vm.Email)
+	if err == nil {
+		return nil, ErrUserAlreadyExists
+	}
+
+	admissionNumber, err := nanoid.New(8)
+	if err != nil {
+		return nil, err
+	}
+
+	s := &Student{
+		Firstname:       vm.Firstname,
+		Lastname:        vm.Lastname,
+		Email:           vm.Email,
+		AdmissionNumber: admissionNumber,
+		Year:            1,
+	}
+
+	entity, err := sh.sr.CreateStudent(ctx, s)
+	if err != nil {
+		return nil, err
+	}
+
+	return parseToStudent(entity), nil
+}
+
+func (sh *StudentHandler) UpdateStudentDetails(ctx context.Context, vm UpdateStudentDetailsVM, ID string) (*Student, error) {
+	studentID := uuid.MustParse(ID)
+
+	s := &Student{
+		Firstname: vm.Firstname,
+		Lastname:  vm.Lastname,
+		Email:     vm.Email,
+	}
+
+	entity, err := sh.sr.UpdateStudentDetails(ctx, s, studentID)
+	if err != nil {
+		return nil, err
+	}
+
+	return parseToStudent(entity), nil
+}
+
+func (sh *StudentHandler) UpdateStudentDepartment(ctx context.Context, vm UpdateStudentDepartmentVM, ID string) (*Student, error) {
+	studentID := uuid.MustParse(ID)
+	departmentID := uuid.MustParse(vm.DepartmentID)
+
+	entity, err := sh.sr.UpdateStudentDepartment(ctx, studentID, departmentID)
+	if err != nil {
+		return nil, err
+	}
+
+	return parseToStudent(entity), nil
+}
+
+func (sh *StudentHandler) DeleteStudent(ctx context.Context, ID string) error {
+	studentID := uuid.MustParse(ID)
+
+	if err := sh.sr.DeleteStudent(ctx, studentID); err != nil {
+		return err
+	}
+
+	return nil
 }
