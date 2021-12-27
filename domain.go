8b6789a38a99c450f82786171a3a53aeebc0355e
Mayowa-Ojo <mayowaojo.e@gmail.com>
2021-12-27 16:01:29 -0700

    feat: define methods on class and staff interface

--- domain.go
+++ domain.go
@@ -1,26 +1,81 @@
-package main
+package college
 
 import (
+	"college/ent"
 	"context"
-	"ent-demo/ent"
+
+	"github.com/google/uuid"
 )
 
 type Datastore struct {
 	c *ent.Client
 }
 
+type GetStudentsVM struct {
+	Page  int `form:"page"`
+	Count int `form:"count"`
+}
+
+type CreateStudentVM struct {
+	Firstname string `json:"firstname"`
+	Lastname  string `json:"lastname"`
+	Email     string `json:"email"`
+}
+
+type UpdateStudentDetailsVM struct {
+	Firstname string `json:"firstname"`
+	Lastname  string `json:"lastname"`
+	Email     string `json:"email"`
+}
+
+type UpdateStudentDepartmentVM struct {
+	DepartmentID string `json:"department_id"`
+}
+
+type CreateDepartmentVM struct {
+	Name      string `json:"name"`
+	Code      string `json:"code"`
+	Telephone string `json:"telephone"`
+}
+
+type GetClassesVM struct {
+	Page  int `form:"page"`
+	Count int `form:"count"`
+}
+
+type ClassRegistrationVM struct {
+	Code string `form:"code"`
+}
+
 type StudentRepository interface {
-	FindListStudents(context.Context) ([]*ent.Student, error)
+	FindListStudents(ctx context.Context, pg *Paginator) ([]*ent.Student, *Paginator, error)
+	FindStudentByEmail(ctx context.Context, email string) (*ent.Student, error)
+	FindStudentByID(ctx context.Context, ID uuid.UUID) (*ent.Student, error)
+	CreateStudent(ctx context.Context, s *Student) (*ent.Student, error)
+	UpdateStudentDetails(ctx context.Context, s *Student, ID uuid.UUID) (*ent.Student, error)
+	UpdateStudentDepartment(ctx context.Context, studentID, departmentID uuid.UUID) (*ent.Student, error)
+	DeleteStudent(ctx context.Context, ID uuid.UUID) error
+	UpdateStudentAddClass(ctx context.Context, sID uuid.UUID, class *ent.Class) (*ent.Student, error)
+	UpdateStudentRemoveClass(ctx context.Context, sID uuid.UUID, classCode string) (*ent.Student, error)
 }
 
 type DepartmentRepository interface {
-	FindListDepartments()
+	FindListDepartments(ctx context.Context) ([]*ent.Department, error)
+	FindListDepartmentsWithStudents(ctx context.Context) ([]*ent.Department, error)
+	FindDepartmentByID(ctx context.Context, ID uuid.UUID) (*ent.Department, error)
+	FindDepartmentByName(ctx context.Context, name string) (*ent.Department, error)
+	CreateDepartment(ctx context.Context, d *Department) (*ent.Department, error)
 }
 
-type CourseRepository interface {
-	// FindListStudents()
+type ClassRepository interface {
+	FindListClasses(ctx context.Context, pg *Paginator) ([]*ent.Class, *Paginator, error)
+	FindClassByCode(ctx context.Context, code string) (*ent.Class, error)
+	// FindListClassesForStudent(ctx context.Context, pg *Paginator) ([]*ent.Class, *Paginator, error)
+	// FindListClassesForDepartment(ctx context.Context, pg *Paginator) ([]*ent.Class, *Paginator, error)
 }
 
-type LecturerRepository interface {
-	// FindListStudents()
+type StaffRepository interface {
+	FindListStaffs(ctx context.Context, pg *Paginator) ([]*ent.Staff, *Paginator, error)
+	FindStaffByID(ctx context.Context, ID uuid.UUID) (*ent.Staff, error)
+	// CreateStaff(ctx context.Context, s *Staff) (*ent.Staff, error)
 }
